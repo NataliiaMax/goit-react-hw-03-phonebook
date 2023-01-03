681d4bdf50e45ab7233f8821c10ef49e1a905729
Nataliia <maksimeyko@ukr.net>
2023-01-03 18:34:37 +0200

    update form

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './AddForm.module.css';
+import PropTypes from 'prop-types';
 
 class ContactForm extends React.Component {
   state = {
@@ -31,32 +32,32 @@ class ContactForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <div className={style.containerInput}>
-        <label className={style.formLabel}>
-          Name
-          <input
-            className={style.formInput}
-            value={name}
-            type="text"
-            onChange={this.handleChange}
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className={style.formLabel}>
-          Number
-          <input
-            className={style.formInput}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
-        </label>
+          <label className={style.formLabel}>
+            Name
+            <input
+              className={style.formInput}
+              value={name}
+              type="text"
+              onChange={this.handleChange}
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={style.formLabel}>
+            Number
+            <input
+              className={style.formInput}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={this.handleChange}
+            />
+          </label>
         </div>
         <button type="submit" className={style.buttonForm}>
           Add contact
@@ -65,5 +66,10 @@ class ContactForm extends React.Component {
     );
   }
 }
+
+ContactForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
